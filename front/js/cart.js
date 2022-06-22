a6fa9a6205cad116a22cfb81bd9ebe6885833c9c
Jeremy Guery <j.guery226@gmail.com>
2022-06-22 10:34:37 +0200

    recfactorisation de la page cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -14,72 +14,33 @@ if (!finalProduct) {
 
     for (let i = 0; i < finalProduct.length; i++) {
 
-        // Création de la balise "article" et insertion dans la section
-        let productArticle = document.createElement("article");
-        document.querySelector("#cart__items").appendChild(productArticle);
-        productArticle.className = "cart__item";
-        productArticle.setAttribute("data-id", finalProduct[i]._id);
+        const product = document.createElement('article');
 
-        // Insertion de l'élément "div" pour l'image produit
-        let productDivImg = document.createElement("div");
-        productArticle.appendChild(productDivImg);
-        productDivImg.className = "cart__item__img";
+        product.innerHTML = `
+        <article class="cart__item" data-id="${finalProduct[i]._id}" data-color="${finalProduct[i].colorChoice}">
+                <div class="cart__item__img">
+                  <img src="${finalProduct[i].imageUrl}" alt="${finalProduct[i].name}">
+                </div>
+                <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                    <h2>${finalProduct[i].name}</h2>
+                    <p>${finalProduct[i].colorChoice}</p>
+                    <p>${finalProduct[i].price * finalProduct[i].quantityChoice + " €"}</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Qté : </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${finalProduct[i].quantityChoice}">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Supprimer</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+        `;
 
-        // Insertion de l'image
-        let productImg = document.createElement("img");
-        productDivImg.appendChild(productImg);
-        productImg.src = finalProduct[i].imageUrl;
-        // productImg.alt = productLocalStorage.altImgProduit;
-
-        // Insertion de l'élément "div" pour la description produit
-        let productItemContent = document.createElement("div");
-        productArticle.appendChild(productItemContent);
-        productItemContent.className = "cart__item__content";
-
-        // Insertion de l'élément "div"
-        let productItemContentTitlePrice = document.createElement("div");
-        productItemContent.appendChild(productItemContentTitlePrice);
-        productItemContentTitlePrice.className = "cart__item__content__titlePrice";
-
-        // Insertion du titre h2
-        let productTitle = document.createElement("h2");
-        productItemContentTitlePrice.appendChild(productTitle);
-        productTitle.innerHTML = finalProduct[i].name;
-
-        // Insertion de la couleur
-        let productColor = document.createElement("p");
-        productTitle.appendChild(productColor);
-        productColor.innerHTML = finalProduct[i].colorChoice;
-
-        // Insertion du prix
-        let productPrice = document.createElement("p");
-        productItemContentTitlePrice.appendChild(productPrice);
-        productPrice.innerHTML = finalProduct[i].price * finalProduct[i].quantityChoice + " €";
-
-        // Insertion de l'élément "div"
-        let productItemContentSettings = document.createElement("div");
-        productItemContent.appendChild(productItemContentSettings);
-        productItemContentSettings.className = "cart__item__content__settings";
-
-        // Insertion de l'élément "div"
-        let productItemContentSettingsQuantity = document.createElement("div");
-        productItemContentSettings.appendChild(productItemContentSettingsQuantity);
-        productItemContentSettingsQuantity.className = "cart__item__content__settings__quantity";
-
-        // Insertion de "Qté : "
-        let productQty = document.createElement("p");
-        productItemContentSettingsQuantity.appendChild(productQty);
-        productQty.innerHTML = "Qté : ";
-
-        // Insertion de la quantité
-        let productQuantity = document.createElement("input");
-        productItemContentSettingsQuantity.appendChild(productQuantity);
-        productQuantity.value = finalProduct[i].quantityChoice;
-        productQuantity.className = "itemQuantity";
-        productQuantity.setAttribute("type", "number");
-        productQuantity.setAttribute("min", "1");
-        productQuantity.setAttribute("max", "100");
-        productQuantity.setAttribute("name", "itemQuantity");
+        document.getElementById('cart__items').appendChild(product);
 
         // Insertion quantitée finaux
         let quantityTotalCalcul = [];
@@ -106,16 +67,9 @@ if (!finalProduct) {
         let finalPriceChoice = document.querySelector("#totalPrice");
         finalPriceChoice.innerHTML = priceTotal;
 
-        // Insertion de l'élément "div"
-        let productItemContentSettingsDelete = document.createElement("div");
-        productItemContentSettings.appendChild(productItemContentSettingsDelete);
-        productItemContentSettingsDelete.className = "cart__item__content__settings__delete";
+        // Supprimer un article
+        let productSupprimer = document.querySelector(".deleteItem");
 
-        // Insertion de "p" supprimer
-        let productSupprimer = document.createElement("p");
-        productItemContentSettingsDelete.appendChild(productSupprimer);
-        productSupprimer.className = "deleteItem";
-        productSupprimer.innerHTML = "Supprimer";
         productSupprimer.addEventListener("click", (e) => {
             e.preventDefault;
 
